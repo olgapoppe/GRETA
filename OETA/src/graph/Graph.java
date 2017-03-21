a7c7e8582368644b636e1b26c8b8968112b88333
olgapoppe <olga.poppe@gmail.com>
2017-03-21 16:36:59 -0400

    minor

--- OETA/src/graph/Graph.java
+++ OETA/src/graph/Graph.java
@@ -67,9 +67,9 @@ public class Graph {
 			nodeNumber++;
 						
 			// Every n'th event marks all previous events as incompatible with all future events
-			if (negated_events_per_window > 0 && number_of_events_per_window > 0) {
+			if (negated_events_per_window > 0) {
 				int n = number_of_events_per_window/(1+negated_events_per_window);
-				if (nodeNumber%n == 0) {
+				if (n > 0 && nodeNumber%n == 0) {
 			
 					System.out.println("Negated event at " + event.sec);
 					for (NodesPerSecond nodes_per_second : all_nodes) {
